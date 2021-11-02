091f3d8e3351aa98e84e0a605b282b3ceb3adaeb
nostalgebraist <nostalgebraist@gmail.com>
2021-11-02 15:56:26 -0700

    fmaps

--- DiffAugment-stylegan2-pytorch/train.py
+++ DiffAugment-stylegan2-pytorch/train.py
@@ -51,6 +51,7 @@ def setup_training_loop_kwargs(
     # nost
     latent_size      = None, # Override latent_size: <int>
     fmax             = None,
+    fmaps            = None,
     pl_weight        = None, # Override path length reg: <float>
     style_mixing_prob= None,
     disable_lazy_reg = None, # disable lazy reg: <bool>
@@ -164,9 +165,13 @@ def setup_training_loop_kwargs(
     if map_override is None:
         map_override = 8
 
+    if fmaps is None:
+        # only affects 'auto'
+        fmaps = 0.5
+
     cfg_specs = {
         'low_shot':  dict(ref_gpus=-1, kimg=300,    mb=8,  mbstd=4,  fmaps=1,   lrate=0.002,  gamma=10,   ema=10,  ramp=None, map=2, snap=10),
-        'auto':      dict(ref_gpus=-1, kimg=300,  mb=-1, mbstd=-1, fmaps=1,  lrate=-1,     gamma=-1,   ema=-1,  ramp=0.05, map=map_override), # Populated dynamically based on resolution and GPU count.
+        'auto':      dict(ref_gpus=-1, kimg=300,  mb=-1, mbstd=-1, fmaps=fmaps,  lrate=-1,     gamma=-1,   ema=-1,  ramp=0.05, map=map_override), # Populated dynamically based on resolution and GPU count.
         'stylegan2': dict(ref_gpus=8,  kimg=25000,  mb=32, mbstd=4,  fmaps=1,   lrate=0.002,  gamma=10,   ema=10,  ramp=None, map=8), # Uses mixed-precision, unlike the original StyleGAN2.
         'paper256':  dict(ref_gpus=8,  kimg=25000,  mb=64, mbstd=8,  fmaps=0.5, lrate=0.0025, gamma=1,    ema=20,  ramp=None, map=8),
         'paper512':  dict(ref_gpus=8,  kimg=25000,  mb=64, mbstd=8,  fmaps=1,   lrate=0.0025, gamma=0.5,  ema=20,  ramp=None, map=8),
@@ -470,6 +475,7 @@ class CommaSeparatedList(click.ParamType):
 # nost
 @click.option('--latent-size', type=int, metavar='INT')
 @click.option('--fmax', type=int, metavar='INT')
+@click.option('--fmaps', type=float)
 @click.option('--pl-weight', type=float)
 @click.option('--style-mixing-prob', type=float)
 @click.option('--disable-lazy-reg', type=bool, metavar='BOOL')
