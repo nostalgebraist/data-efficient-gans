71b642f88b2b5c538428d341a582ff11323ea4c6
nostalgebraist <nostalgebraist@gmail.com>
2021-11-06 15:56:25 -0700

    check if this works now

--- DiffAugment-stylegan2-pytorch/training/training_loop.py
+++ DiffAugment-stylegan2-pytorch/training/training_loop.py
@@ -165,7 +165,7 @@ def training_loop(
         c = torch.empty([batch_gpu, G.c_dim], device=device)
         txt = torch.empty([batch_gpu, 1], device=device, dtype=torch.int)
         img = misc.print_module_summary(G, [z, c, txt])
-        # misc.print_module_summary(D, [img, c, txt])
+        misc.print_module_summary(D, [img, c, txt])
 
     # Setup augmentation.
     if rank == 0:
