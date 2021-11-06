5d1ff341e70f0ddb91d91476d603cb862b421aa7
nostalgebraist <nostalgebraist@gmail.com>
2021-11-06 13:55:54 -0700

    fix

--- DiffAugment-stylegan2-pytorch/training/training_loop.py
+++ DiffAugment-stylegan2-pytorch/training/training_loop.py
@@ -60,8 +60,8 @@ def setup_snapshot_image_grid(training_set, random_seed=0):
             label_groups[label] = [indices[(i + gw) % len(indices)] for i in range(len(indices))]
 
     # Load data.
-    images, labels = zip(*[training_set[i] for i in grid_indices])
-    return (gw, gh), np.stack(images), np.stack(labels)
+    images, labels, txts = zip(*[training_set[i] for i in grid_indices])
+    return (gw, gh), np.stack(images), np.stack(labels), txts
 
 #----------------------------------------------------------------------------
 
@@ -219,11 +219,12 @@ def training_loop(
     grid_c = None
     if rank == 0:
         print('Exporting sample images...')
-        grid_size, images, labels = setup_snapshot_image_grid(training_set=training_set)
+        grid_size, images, labels, txts = setup_snapshot_image_grid(training_set=training_set)
         save_image_grid(images, os.path.join(run_dir, 'reals.png'), drange=[0,255], grid_size=grid_size)
         grid_z = torch.randn([labels.shape[0], G.z_dim], device=device).split(batch_gpu)
         grid_c = torch.from_numpy(labels).to(device).split(batch_gpu)
-        images = torch.cat([G_ema(z=z, c=c, noise_mode='const').cpu() for z, c in zip(grid_z, grid_c)]).numpy()
+        grid_txt = txts
+        images = torch.cat([G_ema(z=z, c=c, txt=txt, noise_mode='const').cpu() for z, c, txt in zip(grid_z, grid_c, grid_txt)]).numpy()
         save_image_grid(images, os.path.join(run_dir, 'fakes_init.png'), drange=[-1,1], grid_size=grid_size)
 
     # Initialize logs.
