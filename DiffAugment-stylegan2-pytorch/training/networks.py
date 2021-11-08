e2a4377a1654e6662a0cb543a451ae07b2a1b38a
nostalgebraist <nostalgebraist@gmail.com>
2021-11-08 13:23:28 -0800

    lower init gain

--- DiffAugment-stylegan2-pytorch/training/networks.py
+++ DiffAugment-stylegan2-pytorch/training/networks.py
@@ -367,7 +367,7 @@ class CrossAttention(torch.nn.Module):
         heads,
         text_dim=512,
         clamp=None,
-        init_gain=1e-1
+        init_gain=1e-2
     ):
         super().__init__()
         self.dim = dim
