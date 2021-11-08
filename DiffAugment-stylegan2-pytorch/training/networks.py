5b9debdd66671833a8eb755b1749d59b446b5136
nostalgebraist <nostalgebraist@gmail.com>
2021-11-07 19:23:14 -0800

    work

--- DiffAugment-stylegan2-pytorch/training/networks.py
+++ DiffAugment-stylegan2-pytorch/training/networks.py
@@ -795,8 +795,9 @@ class DiscriminatorBlock(torch.nn.Module):
             self.conv0 = Conv2dLayer(tmp_channels, tmp_channels, kernel_size=3, activation=activation,
                 trainable=next(trainable_iter), conv_clamp=conv_clamp, channels_last=self.channels_last)
 
-        self.conv1 = Conv2dLayer(tmp_channels, out_channels, kernel_size=3, activation=activation, down=2,
-            trainable=next(trainable_iter), resample_filter=resample_filter, conv_clamp=conv_clamp, channels_last=self.channels_last)
+        if not self.use_encoder_decoder:
+            self.conv1 = Conv2dLayer(tmp_channels, out_channels, kernel_size=3, activation=activation, down=2,
+                trainable=next(trainable_iter), resample_filter=resample_filter, conv_clamp=conv_clamp, channels_last=self.channels_last)
 
         if architecture == 'resnet':
             self.skip = Conv2dLayer(tmp_channels, out_channels, kernel_size=1, bias=False, down=2,
